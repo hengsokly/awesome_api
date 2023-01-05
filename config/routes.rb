0eab688efd32c6f7b65ae3871a35361f017e9e75
Sokly <hengsokly23@gmail.com>
2023-01-05 16:00:55 +0700

    init project

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
+
+  resources :todos
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    email { "MyString" }
+    password { "password" }
+    password_confirmation { "password" }
+  end
+end
